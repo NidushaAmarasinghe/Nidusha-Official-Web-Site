26d9f2271dd22151320f30a10710f5a7bb961ef2
Nidusha Amarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2022-05-25 16:18:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -34,29 +34,17 @@ $(document).ready(function(){
     });
 
     // typing text animation script
-    var typed = new Typed(".typing-1", {
-        strings: ["Bot Developer", "Student", "A friend","Web Deverloper","Web Disigner"],
+    var typed = new Typed(".typing", {
+        strings: ["Web Dev", "Bot Dev", "Student", "Fast Learner"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Hii There!", "You Can Talk Me", "නිද්දා","😘","Thanks!"],
+        strings: ["Web Dev", "Bot Dev", "Student", "Fast Learner"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-3", {
-        strings: ["<b><h1>Hello 💫, You Know Me As Nidusha Amarasinghe</h1></b>"],
-        typeSpeed: 100,
-        backSpeed: 100000000000000000000,
-        loop: true
-    });
-    var typed = new Typed(".typing-4", {
-        strings: ["<br>____ꕤ About Me ꕤ_________<br> ◈──────────────────◈<br>➼ Name :  A.A Nidusha Amarasinghe<br>➼ Username : @NidushaAmarasinghe<br>➼ Contact me : @NidushaChat_Bot <br>➼ Birthday  : 2009/07/03<br>➼ Relationship Status : single with real Friends<br>➼ Who am I:  An a/l  student<br>➼ School : Bandaragama Central <br>➼ City : Bandaragama <br>➼ District » Kaluthara <br>➼ Country » Sri Lanka 🇱🇰<br>◈──────────────────◈</p>"],
-        typeSpeed: 100,
-        backSpeed: 100000000000000000000,
         loop: true
     });
 
